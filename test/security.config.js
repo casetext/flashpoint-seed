6173c0ce4ad73fef0a33165f291ced7d43cad28d
Harry Schmidt <goldibex@casetext.com>
2015-01-23 14:00:48 -0800

    [seed] add Targaryen "users" to global exports for tests

--- test/security.config.js
+++ test/security.config.js
@@ -7,4 +7,5 @@ chai.use(targaryen.chai);
 targaryen.setFirebaseRules(require('../build/rules.json'));
 targaryen.setFirebaseData(require('../build/populate.json'));
 
+global.users = targaryen.users;
 global.expect = chai.expect;
